1533bfe16b5e1a63cf021bb811fad969c39c8402
arno@arnowaschk.de <arno@arnowaschk.de>
2025-04-21 13:14:21 +0200

    insert requested code

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,5 +1,5 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
@@ -7,19 +7,18 @@
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
+    name="Employee"
 
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
-        
+    def names(self):
         # Query 3
         # Write an SQL query
         # that selects two columns 
@@ -27,14 +26,22 @@
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
+        query_str=f"""
+            SELECT full_name, employee_id FROM (
+                SELECT employee_id, first_name , 
+                last_name, 
+                CONCAT(first_name, ' ', last_name) AS full_name
+                FROM employee
+                )
+            """
+        return self.query(query_str)
     
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def username(self,id):
         
         # Query 4
         # Write an SQL query
@@ -42,8 +49,16 @@
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
-
+        query_str=f"""
+            SELECT full_name FROM (
+                SELECT first_name , 
+                last_name, 
+                CONCAT(first_name, ' ', last_name) AS full_name
+                FROM employee
+                WHERE employee.employee_id = {id}
+                )
+            """
+        return self.query(query_str)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -52,10 +67,9 @@
     # so when it is called, a pandas dataframe
     # is returns containing the execution of
     # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        query_string=f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +77,4 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+        return self.pandas_query(query_string)
--- python-package/employee_events/sql_execution.py
+++ python-package/employee_events/sql_execution.py
@@ -1,13 +1,13 @@
 from sqlite3 import connect
 from pathlib import Path
 from functools import wraps
+from importlib import resources
 import pandas as pd
 
 # Using pathlib, create a `db_path` variable
 # that points to the absolute path for the `employee_events.db` file
-#### YOUR CODE HERE
-
-
+with resources.path("employee_events", "employee_events.db") as p:
+    db_path=Path(p).resolve()
 # OPTION 1: MIXIN
 # Define a class called `QueryMixin`
 class QueryMixin:
@@ -16,14 +16,23 @@ class QueryMixin:
     # that receives an sql query as a string
     # and returns the query's result
     # as a pandas dataframe
-    #### YOUR CODE HERE
+    def pandas_query(self,query_string:str):
+        connection=connect(db_path)
+        df=pd.read_sql_query(query_string,connection)
+        connection.close()
+        return df
 
     # Define a method named `query`
     # that receives an sql_query as a string
     # and returns the query's result as
     # a list of tuples. (You will need
     # to use an sqlite3 cursor)
-    #### YOUR CODE HERE
+    def query(self,query_string:str):
+        connection=connect(db_path)
+        cursor = connection.cursor()
+        result = cursor.execute(query_string).fetchall()
+        connection.close()
+        return result
     
 
  
